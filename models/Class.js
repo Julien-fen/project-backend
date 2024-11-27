e1d293363c6cc01b221f3281fb0696be6d79f982
Julien-fen <julien.fenelon@gmail.com>
2024-11-27 18:43:04 -0500

    Create Class.js

---
+++ models/Class.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Class = sequelize.define('Class', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  number: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+  },
+});
+
+module.exports = Class;
