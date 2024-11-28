18bab12b039886e733e1b60924be3a57ba3c2232
Julien-fen <julien.fenelon@gmail.com>
2024-11-27 22:39:41 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,4 @@
-require('dotenv').config();  // Charge les variables d'environnement à partir du fichier .env
-
-const { Sequelize } = require('sequelize'); 
-
-// Vérification de la variable DATABASE_URL
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL n'est pas définie. Vérifiez votre fichier .env ou vos variables d'environnement.");
-}
+const { Sequelize } = require('sequelize');
 
 // Connexion à PostgreSQL avec les variables d'environnement
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
